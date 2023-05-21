b063cf0b3718e7501cb0da7d33abdb6a06142273
Anass BOURASSE <50037710+BOURASSEAnass@users.noreply.github.com>
2023-05-21 02:26:25 +0100

    Update index.js

--- spam-checker-app/pages/index.js
+++ spam-checker-app/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <div className="flex flex-col md:px-12 px-0 relative bg-background font-poppins items-center min-h-screen">
       <h1 className="text-6xl font-bold text-primary mt-10">
-        <span className="text-active">Spam</span> Checker
+        <span className="text-active">AI GPT-4</span> Detector
       </h1>
       <h2 className="text-primary text-2xl font-light mt-6">
         Check any text for spam
